423756cbcfc076d15d1dfb1167c8f60a1814ec85
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2019-02-04 14:30:50 -0800

    update page

--- site.hs
+++ site.hs
@@ -53,6 +53,7 @@ main = hakyll $ do
   match "lectures/00-*"    $ crunchWithCtxCustom "final" postCtx
   match "lectures/01-*"    $ crunchWithCtxCustom "final" postCtx
   match "lectures/02-*"    $ crunchWithCtxCustom "final" postCtx
+  match "lectures/03-*"    $ crunchWithCtxCustom "final" postCtx
   match "lectures/*"       $ crunchWithCtxCustom "lecture" postCtx
   match "assignments/*"    $ crunchWithCtx postCtx
   match "templates/*"      $ compile templateCompiler
